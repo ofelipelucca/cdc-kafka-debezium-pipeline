3dfdaad1a87039049d045f8cf1f22aac9fe86be2
Felipe Lucca Taumaturgo de Oliveira <felipe.oliveira@4mti.com.br>
2026-05-12 08:33:04 -0300

    feat: reorder fields in PostCreate and PostResponse schemas for consistency

--- api/app/schemas/post.py
+++ api/app/schemas/post.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
+class PostCreate(BaseModel):
+    user_guid: str
+    content: str
+
+
 class PostResponse(BaseModel):
     guid: str
     content: str
     user_guid: str
     created_at: datetime
-
-
-class PostCreate(BaseModel):
-    user_guid: str
-    content: str
